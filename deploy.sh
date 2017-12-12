e8ed6722c9b67b42927341efc7bd897c89f3afcc
Timofey Tipishev <tipishev@gmail.com>
2017-12-12 09:33:39 +0100

    add swap windows shortcut to tmux.conf

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+DIR="~/dotfiles"
+DOTFILES="tmux.conf vimrc bash*"
+
 cd
 
-HITLIST="~/dotfiles/tmux.conf ~/dotfiles/vimrc"
-for file in $HITLIST ~/dotfiles/bash*; do echo rm -f "~/.${file##*/}"; echo ln -s "$file" "~/.${file##*/}"; done
+for file in $DOTFILES; do echo rm -f "~/.${file##*/}"; echo ln -s "$DIR/$file" "~/.${file##*/}"; done
 echo ">~/.hushlogin"
-echo "cat dotfiles/virtualenvs.postdeactivate >>.virtualenvs/postdeactivate"
 echo "cat ~/dotfiles/gitconfig > ~/.gitconfig"
 
-# Config files
-# echo "cp -aT ~/dotfiles/config/terminator/ ~/.config/terminator/"
+# Vim configuration
+VIM_DIR="$DIR/vim/"
+echo "cp -aT $VIM_DIR/ftdetect/ ~/.vim/ftdetect/"
+echo "cp -aT $VIM_DIR/ftplugin/ ~/.vim/ftplugin/"
+echo "cp -aT $VIM_DIR/UltiSnips/ ~/.vim/UltiSnips/"
+
 # echo "cp -a ~/dotfiles/config/sublime-text-3/* ~/.config/sublime-text-3/Packages/User/"
+
+# TODO one-liners from bin
+# TODO vim dir
