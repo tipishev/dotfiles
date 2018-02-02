4a6ff85930640a175435966d269cb4261ef0126e
Timofey Tipishev <tipishev@gmail.com>
2018-02-02 07:06:49 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ cd
 
 for file in $DOTFILES; do echo rm -f "~/.${file##*/}"; echo ln -s "$DIR/$file" "~/.${file##*/}"; done
 echo ">~/.hushlogin"
-echo "cat ~/dotfiles/gitconfig > ~/.gitconfig"
 
 # Vim configuration
 VIM_DIR="$DIR/vim"
+echo "mkdir ~/.vim"
 echo "cp -aT $VIM_DIR/ftdetect/ ~/.vim/ftdetect/"
 echo "cp -aT $VIM_DIR/ftplugin/ ~/.vim/ftplugin/"
 echo "cp -aT $VIM_DIR/UltiSnips/ ~/.vim/UltiSnips/"
