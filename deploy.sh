ac14aebd5ce48afdd0a61da2ca367fe255f1d982
Timofey Tipishev <tipishev@gmail.com>
2018-10-03 00:08:34 +0200

    remove bash* from deploy list

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="~/dotfiles"
-DOTFILES="tmux.conf vimrc bash* flake8 gitconfig Xmodmap gitignore_global ctags bash_logout bash_prompt bash_profile bashrc bash_aliases lynxrc"
+DOTFILES="tmux.conf vimrc flake8 gitconfig Xmodmap gitignore_global ctags bash_logout bash_prompt bash_profile bashrc bash_aliases lynxrc"
 
 cd
 
